56597f550d0cc94edfde9ef8f8ee35b7d2d0ab62
Tony Greenberg <tonygreen2655@gmail.com>
2017-11-16 14:11:58 -0500

    deleted sampleSNPs

--- sampleSNPs/html/search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['randraw',['RanDraw',['../classsamp_files_1_1_ran_draw.html',1,'sampFiles']]]
-];
--- sampleSNPs/html/search/classes_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['tpedfile',['TpedFile',['../classsamp_files_1_1_tped_file.html',1,'sampFiles']]],
-  ['tpedfilei',['TpedFileI',['../classsamp_files_1_1_tped_file_i.html',1,'sampFiles']]],
-  ['tpedfileo',['TpedFileO',['../classsamp_files_1_1_tped_file_o.html',1,'sampFiles']]]
-];
